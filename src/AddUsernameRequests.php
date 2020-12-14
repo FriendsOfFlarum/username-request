412e4bc98f965983b6d0feab82e83b7a9cc22af7
Ian Morland <imorland@users.noreply.github.com>
2020-12-14 13:19:09 +0000

    Apply fixes from StyleCI

--- src/AddUsernameRequests.php
+++ src/AddUsernameRequests.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/username-request.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\UserRequest;
 
 use Flarum\Api\Controller\ShowForumController;
