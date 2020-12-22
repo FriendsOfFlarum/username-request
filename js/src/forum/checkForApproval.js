deb52726d758baf97411fa00fc42da0eab77b06c
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-21 21:01:02 -0500

    Fix nickname attr not being passed to ResultsModal

--- js/src/forum/checkForApproval.js
+++ js/src/forum/checkForApproval.js
@@ -15,11 +15,11 @@ import ResultsModal from './components/ResultsModal';
 export default function () {
     return new Promise(() => {
         setTimeout(() => {
-            if (app.session.user &&
-                (app.session.user.lastNicknameRequest() && app.session.user.lastNicknameRequest().status() !== 'Sent' ||
-                app.session.user.lastUsernameRequest() && app.session.user.lastUsernameRequest().status() !== 'Sent')
+            const approvedNickname = app.session.user.lastNicknameRequest() && app.session.user.lastNicknameRequest().status() !== 'Sent';
+            const approvedUsername = app.session.user.lastUsernameRequest() && app.session.user.lastUsernameRequest().status() !== 'Sent'
+            if (app.session.user && (approvedNickname || approvedUsername)
             ) {
-                app.modal.show(ResultsModal);
+                app.modal.show(ResultsModal, {nickname: approvedNickname});
             }
         }, 1000);
     });
