405706f651b92459afdcafb744dd4bbda52b830d
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-11-09 15:49:06 +0100

    chore: be more surgical

--- js/src/forum/models/UsernameRequest.ts
+++ js/src/forum/models/UsernameRequest.ts
@@ -1,4 +1,6 @@
 import Model from 'flarum/common/Model';
+import computed from 'flarum/common/utils/computed';
+
 import type User from 'flarum/common/models/User';
 
 export default class UsernameRequest extends Model {
@@ -22,26 +24,13 @@ export default class UsernameRequest extends Model {
     return Model.attribute<boolean>('forNickname').call(this);
   }
 
-  _requestedUsername(value?: string | null) {
-    // @ts-ignore Flarum attribute funcs accept 0 or 1 arg
-    return Model.attribute<string | null>('_requestedUsername').call(this, value);
+  _requestedUsername() {
+    return Model.attribute<string | null>('requestedUsername').call(this);
   }
 
-  requestedUsername(): string | null;
-  requestedUsername(value: string | null): this;
-  requestedUsername(value?: string | null): string | null | this {
-    if (arguments.length) {
-      this._requestedUsername(value ?? null);
-      return this;
-    }
-
-    const newName = this._requestedUsername();
-    if (newName !== null) return newName;
-
-    if (!this.forNickname()) return newName;
-
-    const user = this.user() as User | undefined;
-
-    return user?.username() ?? null;
+  requestedUsername() {
+    return computed('_requestedUsername', 'forNickname', 'user', function (newName, forNickname, user) {
+      return newName === null && forNickname ? (user as User).username() : newName;
+    }).call(this);
   }
 }
